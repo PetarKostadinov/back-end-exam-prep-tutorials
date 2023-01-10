ccba1c77def59341e676db46c3bef676d3d00756
PetarKostadinov <petarkostadinov30@gmail.com>
2023-01-10 22:49:48 +0200

    Adding "strictQuery", false to the database sett

--- config/database.js
+++ config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-
 //TODO change database acording to assignment
 const CONECTION_STRING = 'mongodb://localhost:27017/tutorials';
-
+mongoose.set("strictQuery", false);
 module.exports = async (app) => {
     try {
         await mongoose.connect(CONECTION_STRING, {
